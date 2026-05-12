60b92c802f9accd8fee5af995f5f4dba7742159c
Naveen Kumar <naveenjangid178@gmail.com>
2026-05-12 22:11:29 +0530

    custom linked-list

---
+++ linked-list/CustomLL.java
@@ -0,0 +1,18 @@
+public class CustomLL {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(100, 3);
+
+        list.display();
+        
+        System.out.println(list.deleteFirst());
+        System.out.println(list.deleteLast());
+        System.out.println(list.delete(2));
+        list.display();
+    }
+}
