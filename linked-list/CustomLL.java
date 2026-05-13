3a5af2443752de5366327554eb2e68a82348f9e2
Naveen Kumar <naveenjangid178@gmail.com>
2026-05-13 21:38:29 +0530

    custom doubly and circulor linked-list

--- linked-list/CustomLL.java
+++ linked-list/CustomLL.java
@@ -1,18 +1,37 @@
 public class CustomLL {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(99);
-        list.insert(100, 3);
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(2);
+        // list.insertFirst(8);
+        // list.insertFirst(17);
+        // list.insertLast(99);
+        // list.insert(100, 3);
 
-        list.display();
+        // list.display();
         
-        System.out.println(list.deleteFirst());
-        System.out.println(list.deleteLast());
-        System.out.println(list.delete(2));
+        // System.out.println(list.deleteFirst());
+        // System.out.println(list.deleteLast());
+        // System.out.println(list.delete(2));
+        // list.display();
+
+        // DLL list = new DLL();
+        // list.insertFirst(3);
+        // list.insertFirst(2);
+        // list.insertFirst(8);
+        // list.insertFirst(17);
+        // list.insertLast(99);
+        // list.insert(99, 65);
+
+        // list.display();
+
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.display();
+        list.delete(19);
         list.display();
     }
 }
